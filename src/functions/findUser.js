71c08c6c30b0fec25e67098122cf32561b1747a8
Yura Levin <yuraevo@gmail.com>
2020-03-01 15:06:30 +0200

    add keyboards

--- src/functions/findUser.js
+++ src/functions/findUser.js
@@ -2,22 +2,37 @@ const DB = require("../connect_db")
 const Client  = require('pg');
 const bot = require("../bot") 
 const registration = require("./userRegistration")
+const keyboard_text = require("../keyboard_text")
+const keyboards = require("../keyboard")
+const text = require("../text")
 
 async function findUser(id, first_name, last_name, username) {
     try {
+        console.log("Зашло в findUser")
         database = new Client.Pool(DB)
         var queryUser = 'SELECT username FROM public."User" WHERE username = ($1)'
         const user = await database.query(queryUser, [username])
         console.table(user.rows)
         if(user.rows[0] != null) {
             bot.sendMessage(id, "Добро пожаловать, " + user.rows[0].username + "!", {
-                parse_mode: "Markdown"
+                parse_mode: "Markdown",
+                reply_markup: {
+                    keyboard: keyboards.personal_accaunt
+                }
             });
         }
         else {
-            bot.sendMessage(id, "Вы не зарегистрированы", {
-                parse_mode: "Markdown"
+            bot.sendMessage(id, text.choiseCountry, {
+                parse_mode: "HTML",
+                reply_markup: {
+                    inline_keyboard:
+                    [
+                        [{ text: "Ukraine", callback_data: "1" }]
+                    ]
+                }
             });
+
+            //registration.userRegistration(id, first_name, last_name,)
         }
     }
     catch (ex) 
