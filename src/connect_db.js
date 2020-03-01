7eab088b4ae68d52acd55700ffa3a3aef6406b1f
Yura Levin <yuraevo@gmail.com>
2020-03-01 13:32:00 +0200

    remove connection

--- src/connect_db.js
+++ src/connect_db.js
@@ -1,12 +1,12 @@
 const { Client } = require('pg');
 const SEQURITY_INFORMATION = require("./token")
 
-const client = new Client({
+const client = {
     user: SEQURITY_INFORMATION.USER,
     password: SEQURITY_INFORMATION.PASSWORD,
     host: SEQURITY_INFORMATION.HOST,
     port: SEQURITY_INFORMATION.PORT,
     database: SEQURITY_INFORMATION.DATABASE
-})
+}
 
 module.exports = client
