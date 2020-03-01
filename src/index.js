c9748885072a33e196b2ca5afef9a275edfff9c7
Yura Levin <yuraevo@gmail.com>
2020-03-01 14:29:54 +0200

    passing arguments

--- src/index.js
+++ src/index.js
@@ -2,19 +2,21 @@ const bot = require("./bot")
 const bot_command = require("./bot_commands")
 const keyboard = require("./keyboard")
 const keyboard_text = require("./keyboard_text")
-const func = require("./functions")
+const func = require("./functions/findUser")
 
 bot_command()
 
 bot.on("message", msg => {
     const { id } = msg.chat
-    const { first_name } = msg.chat
     const { username } = msg.chat
+    const { first_name } = msg.chat
+    const { last_name } = msg.chat
+
     console.log("зашло")
     switch(msg.text) {
         case keyboard_text.main.personal_accaunt:
-            console.log("Пользователь заходит в аккаунт")
-            func.findUser(id, username)
+            console.log("Пользователь " + username + " заходит в личный аккаунт")
+            func.findUser(id, first_name, last_name, username)
         break    
     }
 })
