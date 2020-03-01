a8100683b81de1b684c4634dfeb1ffdc3c6e8e99
Yura Levin <yuraevo@gmail.com>
2020-03-01 13:32:44 +0200

    render functions separately

--- src/index.js
+++ src/index.js
@@ -1,46 +1,20 @@
 const bot = require("./bot") 
-const DB = require("./connect_db")
 const bot_command = require("./bot_commands")
 const keyboard = require("./keyboard")
 const keyboard_text = require("./keyboard_text")
- 
+const func = require("./functions")
+
 bot_command()
 
-allClients()
-
-async function findUser(id, first_name) {
-    try {
-        await DB.connect()
-        const result = await DB.query("SELECT name FROM client WHERE name = ($1)", [first_name])
-        bot.sendMessage(id, result.rows[0].name, {
-            parse_mode: "Markdown"
-        });
+bot.on("message", msg => {
+    const { id } = msg.chat
+    const { first_name } = msg.chat
+    const { username } = msg.chat
+    console.log("зашло")
+    switch(msg.text) {
+        case keyboard_text.main.personal_accaunt:
+            console.log("Пользователь заходит в аккаунт")
+            func.findUser(id, username)
+        break    
     }
-    catch (ex) 
-    {
-        console.log('Something wrong happend: ' + ex)
-    }
-    finally {
-        await DB.end()
-        console.log("DB disconnect")
-    }
-}
-async function allClients() {
-    try 
-    {
-        bot.on('message', msg => {
-            const { id } = msg.chat
-            const { first_name } = msg.chat
-            const { username } = msg.chat
-            console.log(id)
-            console.log(username)
-            findUser(id, first_name)
-        })
-    }
-    catch (ex) 
-    {
-        console.log('Something wrong happend: ' + ex)
-    }
-    finally {
-    }
-}
+})
