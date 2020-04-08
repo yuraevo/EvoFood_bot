fb852b578889c47f842bd0dd46b0d2b6f64ed727
Yura Levin <yuraevo@gmail.com>
2020-04-09 01:19:22 +0300

    choice city

--- src/functions/choiceAdress.js
+++ src/functions/choiceAdress.js
@@ -1,25 +1,39 @@
 const DB = require("../connect_db")
 const Client  = require('pg');
-const bot = require("../bot") 
+//const bot = require("../bot") 
 const registration = require("./userRegistration")
 const keyboard_text = require("../keyboard_text")
 const keyboards = require("../keyboard")
 const text = require("../text")
 
 
-async function choiceAdress(id, data, username) {
+async function choiceAdress(id, data, username, bot) {
     try {
-        switch(query.data) {
-            case "Ukraine_Odessa":
-                bot.sendMessage(query.message.chat.id, "Вы выбрали Украину!", {})
-                //console.log(username + "Выбрал Украина-Одесса")
-            break
-        }
+        console.log("Зашло в choiseAdress")
+        console.log(data)
+            switch(data) {
+                case "Odessa":
+                    // if(err){
+                    //     return console.error('Соединение не удалось')
+                    // }
+                    database = new Client.Pool(DB)
+                    var choiceOdessa = 'INSERT INTO public."City"(country, name_city) VALUES ($1, $2)'
+                    const insert = await database.query(choiceOdessa, [1, 'street'])
+                    bot.sendMessage(id, "Вы выбрали Одессу\uD83C\uDDFA\uD83C\uDDE6\u2693" + "!", {
+                        parse_mode: "Markdown",
+                        reply_markup: {
+                            keyboard: keyboards.personal_accaunt
+                            }
+                        }); 
+            }
     }
     catch(ex) {
-
+        console.log('Something wrong happend - ' + ex)
     }
     finally {
-
+        await database.end()
+        console.log("DB disconnect choiseAdress")
     }
 }
+
+module.exports.choiceAdress = choiceAdress;
