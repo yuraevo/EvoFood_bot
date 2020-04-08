de4c337cc8dd620fbae7b288c577f90513c542b3
Yura Levin <yuraevo@gmail.com>
2020-04-09 02:20:23 +0300

    choise country

--- src/functions/choiceAdress.js
+++ src/functions/choiceAdress.js
@@ -1,30 +1,28 @@
 const DB = require("../connect_db")
 const Client  = require('pg');
-//const bot = require("../bot") 
 const registration = require("./userRegistration")
 const keyboard_text = require("../keyboard_text")
 const keyboards = require("../keyboard")
 const text = require("../text")
 
 
-async function choiceAdress(id, data, username, bot) {
+async function choiceAdress(id, data, username, bot, query) {
     try {
         console.log("Зашло в choiseAdress")
         console.log(data)
             switch(data) {
-                case "Odessa":
-                    // if(err){
-                    //     return console.error('Соединение не удалось')
-                    // }
+                case "Ukraine":
                     database = new Client.Pool(DB)
-                    var choiceOdessa = 'INSERT INTO public."City"(country, name_city) VALUES ($1, $2)'
-                    const insert = await database.query(choiceOdessa, [1, 'street'])
-                    bot.sendMessage(id, "Вы выбрали Одессу\uD83C\uDDFA\uD83C\uDDE6\u2693" + "!", {
-                        parse_mode: "Markdown",
-                        reply_markup: {
-                            keyboard: keyboards.personal_accaunt
-                            }
-                        }); 
+                    var choiceUkraine = 'SELECT name_country FROM public."Country" WHERE name_country = ($1)'
+                    const insert = await database.query(choiceUkraine, ['Ukraine'])
+                    bot.answerCallbackQuery(query.id, "Вы выбрали " + "Ukraine")
+                    // bot.sendMessage(id, "Вы выбрали Одессу\uD83C\uDDFA\uD83C\uDDE6\u2693" + "!", {
+                    //     parse_mode: "Markdown",
+                    //     reply_markup: {
+                    //         keyboard: keyboards.personal_accaunt
+                    //         }
+                    //     }); 
+                break
             }
     }
     catch(ex) {
